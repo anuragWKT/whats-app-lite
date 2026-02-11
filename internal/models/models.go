c931f184b2a56f3b5900023422efbba73fe5b48c
anuragWKT <anurag.s@webknot.in>
2026-02-11 22:33:07 +0530

    created the message and the user models in models.go

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,1 +1,14 @@
 package models
+//the single piece of data exchanged between the client and the server
+type Message struct{
+	Type string `json:"type"`//indicates the action like join,leave,message,typing,private
+	Sender string `json:"sender"`
+	Recipient string `json:"recipient,omitempty"`
+	Content string `json:"content"`
+	Room string `json:"room"`
+}
+//represents the registered user for the authentication
+type User struct{
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
