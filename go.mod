597be6ec3e932ee3404d395b4d1ba9a0f6ad3714
anuragWKT <anurag.s@webknot.in>
2026-02-13 11:40:06 +0530

    implemented the client go routine with readpump and writepump

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module whats-app-lite
 
 go 1.25.7
+
+require (
+	github.com/gorilla/websocket v1.5.3 // indirect
+	golang.org/x/text v0.34.0 // indirect
+)
--- internal/client/client.go
+++ internal/client/client.go
@@ -1,1 +1,97 @@
 package client
+
+import (
+	"bytes"
+	"log"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+const (
+	writeWait      = 10 * time.Second
+	pongWait       = 60 * time.Second
+	pingPeriod     = (pongWait * 9) / 10
+	maxMessageSize = 512
+)
+
+var (
+	newline = []byte{'\n'}
+	space   = []byte{' '}
+)
+
+type Client struct {
+	Conn       *websocket.Conn
+	Send       chan []byte
+	broadcast  chan<- []byte
+	unregister chan<- *Client
+}
+func NewClient(conn *websocket.Conn, broadcast chan<- []byte, unregister chan<- *Client) *Client {
+	return &Client{
+		Conn:       conn,
+		Send:       make(chan []byte, 256),
+		broadcast:  broadcast,
+		unregister: unregister,
+	}
+}
+
+func (c *Client) ReadPump() {
+	defer func() {
+		c.unregister <- c
+		c.Conn.Close()
+	}()
+
+	c.Conn.SetReadLimit(maxMessageSize)
+	c.Conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.Conn.SetPongHandler(func(string) error {
+		c.Conn.SetReadDeadline(time.Now().Add(pongWait))
+		return nil
+	})
+	for {
+		_, message, err := c.Conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error :%v", err)
+			}
+			break
+		}
+		message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
+		c.broadcast <- message
+	}
+}
+
+func (c *Client) WritePump() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.Conn.Close()
+	}()
+	for {
+		select {
+		case message, ok := <-c.Send:
+			c.Conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				c.Conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+			w, err := c.Conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write(message)
+			n := len(c.Send)
+			for i := 0; i < n; i++ {
+				w.Write(newline)
+				w.Write(<-c.Send)
+			}
+			if err := c.Conn.Close(); err != nil {
+				return
+			}
+		case <-ticker.C:
+			c.Conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.Conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
+}
