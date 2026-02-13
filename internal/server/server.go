e3cbf17f9e8a4ff1cb0a75ab8d599a89ca190fa4
anuragWKT <anurag.s@webknot.in>
2026-02-13 12:24:35 +0530

    created the hub|server file

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,37 @@
 package server
+
+import(
+	"sync"
+	"whats-app-lite/internal/room"
+)
+
+type Hub struct{
+	rooms map[string]*room.Room
+	mu sync.RWMutex
+}
+
+func NewHub() *Hub{
+	return  &Hub{
+		rooms:make(map[string]*room.Room),
+	}
+}
+
+func (h *Hub) GetOrCreateRoom(name string) *room.Room{
+	h.mu.RLock()
+	if r,ok:=h.rooms[name]; ok{
+		h.mu.RUnlock()
+		return r
+	}
+	h.mu.RUnlock()
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	if r,ok:=h.rooms[name];ok{
+		return r
+	}
+	newRoom:=room.NewRoom(name)
+	go newRoom.Run()
+
+	h.rooms[name]=newRoom
+	return  newRoom
+}
